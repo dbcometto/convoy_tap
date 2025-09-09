d556cb2dc9bac8c299bbe1b71598fcabc6ed5a17
dbcometto <111076949+dbcometto@users.noreply.github.com>
2025-09-09 14:20:23 -0400

    fixed copied example code

--- ts_convoy_comm/ts_convoy_comm/ts_sub.py
+++ ts_convoy_comm/ts_convoy_comm/ts_sub.py
@@ -80,7 +80,7 @@ def main():
     # Destroy the node explicitly
     # (optional - otherwise it will be done automatically
     # when the garbage collector destroys the node object)
-    minimal_subscriber.destroy_node()
+    convoy_sub.destroy_node()
     rclpy.shutdown()
 
 
